0a0b4dc2051004768c7cee7ced8254aeb06bf957
Liutciia <157923146+Liutciia@users.noreply.github.com>
2024-05-02 07:56:50 +0000

    web scraping

--- src/app.py
+++ src/app.py
@@ -5,42 +5,56 @@ import time
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-
-
-#Step 1: Install dependencies - pip install pandas requests
-
-#Step 2: Download HTML 
-url = 'https://es.wikipedia.org/wiki/Leucocito'  
+# Step 1: Fetch the HTML content of the webpage
+url = "https://es.wikipedia.org/wiki/Leucocito"
 response = requests.get(url)
-html_content = response.text
 
-# Step 3: transform the HTML
-soup = BeautifulSoup(html_content, 'html.parser')
+# Step 2: Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.text, "html")
 
-tables = soup.find_all('table')
+# Step 3: Find a table elements in the parsed HTML
+table = soup.find("table", class_= "wikitable")
+
+# Create a DataFrame
+wikitable = pd.DataFrame(columns=["Tipo", "Porcentaje aproximado en adultos"])
+
+for row in table.find_all("tr"):
+    cell = row.find_all("td")
+    if (cell != []):
+        Tipo = cell[0].text
+        Porcentaje_aproximado_en_adultos = cell[3].text
+        if "%" in Porcentaje_aproximado_en_adultos:
+            wikitable = wikitable.append({"Tipo": Tipo, "Porcentaje aproximado en adultos": Porcentaje_aproximado_en_adultos}, ignore_index = True)
+
+print(wikitable)
+
+#Step 5: 
+#Store the data in sqlite
+connection = sqlite3.connect("wikitable")
+connection.execute("DROP TABLE wikitable")
+
+#Create a table
+cursor = connection.cursor()
+connection.execute("""CREATE TABLE wikitable (
+               Tipo TEXT NOT NULL, 
+               Porcentaje aproximado en adultos TEXT NOT NULL
+               );""")
+
+#Insert the values
+connection.execute("""INSERT INTO wikitable VALUES ("Neutrófilo", "62%")""")
+connection.execute("""INSERT INTO wikitable VALUES ("Eosinófilo", "2.3%")""")
+connection.execute("""INSERT INTO wikitable VALUES ("Basófilo", "0.4%")""")
+connection.execute("""INSERT INTO wikitable VALUES ("Linfocito", "30%")""")
+connection.execute("""INSERT INTO wikitable VALUES ("Monocito", "5.3%")""")
 
 
+#Almacena (commit) los cambios
+connection.commit()
 
+#SELECT
+result_dataFrame = pd.read_sql("Select * from wikitable;", connection)
+print(result_dataFrame)
 
-
-
-# Step 4: Extract data from each table and store it in a DataFrame
-dfs = []
-for table in tables:
-    # Extract data from the table rows and cells
-    data = []
-    rows = table.find_all('tr')
-    for row in rows:
-        row_data = [cell.get_text(strip=True) for cell in row.find_all(['th', 'td'])]
-        data.append(row_data)
-    
-    # Create a DataFrame from the extracted data
-    df = pd.DataFrame(data[1:], columns=data[0])
-    dfs.append(df)
-
-# Concatenate all DataFrames into a single DataFrame
-final_df = pd.concat(dfs, ignore_index=True)
-
-# Display the final DataFrame
-print(final_df)
+#Step 6: Visualize the data
